16608368687123b0ed8c3e56ee9fc6620bd7c544
Lagartijah <simplementebasico@gmail.com>
2021-05-04 19:18:14 -0300

    V1 solo HTML

---
+++ Calculadora/php/menu.php
@@ -0,0 +1,11 @@
+<?php
+include("funciones.php"); //Conecta este el menú con las funciones
+if(isset($_REQUEST['calcular'])){
+
+    //Creación de variables
+    $num1 = $_REQUEST['numero1'];
+    $num2 = $_REQUEST['numero2'];
+    $op = $_REQUEST['selector'];
+    echo "<script> alert('Suma realizada correctamente $num1 / $num2'); window.location.href='../index.html'; </script>";
+}
+?>
