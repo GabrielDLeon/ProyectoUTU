f204fd11dd2470b467c05c22c1c1d3814ae038a1
GabrielDLeon <noesgabriel7574@gmail.com>
2021-05-06 18:12:25 -0300

    Operaciones complejas terminadas

--- Calculadora/php/operaciones.php
+++ Calculadora/php/operaciones.php
@@ -22,8 +22,32 @@ class matematica {
 
     //Operaciones complejas:
 
+    public static function potencia($base,$exponente){
+        $potencia=pow($base,$exponente);
+        return $potencia;
+    }
 
-    
+    public static function raizCuadrada($num){
+        if ($num > 0){
+            $resultado=sqrt($num);
+            return $resultado;
+        } else{
+            return "No se pudo realizar el calculo.";
+        }
+    }
+
+    public static function baskara($a,$b,$c){
+        if (sqrt($b*$b-4*$a*$c)>0){
+
+            $raiz = sqrt($b*$b-4*$a*$c);
+            $solucion1 = (($b*-1)+$raiz)/(2*$a);
+            $solucion2 = (($b*-1)-$raiz)/(2*$a);
+            return "| Raíz 1 = $solucion1 | Raíz 2 = $solucion2 |";
+        } else{
+            return "No tiene solución real";
+        }
+    }
+
     //Operaciones geometricas:
 
 
