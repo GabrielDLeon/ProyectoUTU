c9441202fae5da4c3b1fd26ebbccbacda4fc5100
GabrielDLeon <noesgabriel7574@gmail.com>
2021-05-31 17:11:40 -0300

    Parte 1 completa
    Co-Authored-By: emamaq <83661707+emamaq@users.noreply.github.com>

--- Laboratorio III/php/controlador.php
+++ Laboratorio III/php/controlador.php
@@ -2,9 +2,26 @@
 include("operaciones.php");
 
 if(isset($_REQUEST['btmPart1'])){
+    $num1 = $_REQUEST['val1Part1'];
+    $base1 = $_REQUEST['base1Part1'];
+    $num2 = $_REQUEST['val2Part1'];
+    $base2 = $_REQUEST['base2Part1'];
+    $op = $_REQUEST['operacion'];
 
-    //Líneas de código
-
+    switch($op){
+        case 1:
+            operacion::sumar($num1,$num2,$base1,$base2);
+            break;
+        case 2:
+            operacion::restar($num1,$num2,$base1,$base2);
+            break;
+        case 3:
+            operacion::multiplicar($num1,$num2,$base1,$base2);
+            break;
+        case 4:
+            operacion::dividir($num1,$num2,$base1,$base2);
+            break;
+    }
 }
     
 if(isset($_REQUEST['btmPart2'])){     
--- Laboratorio III/php/operaciones.php
+++ Laboratorio III/php/operaciones.php
@@ -1,13 +1,25 @@
 <?php
 class operacion{
 
-    //Comprobador de existencia
-    public static function comprobadorExistencia($valor){
-        if ($valor == ""){
-            return false;
-        } else if ($valor != ""){
-            return true;
-        }
+    public static function sumar($num1, $num2, $base1, $base2){
+        $resultado = base_convert($num1,$base1,10) + base_convert($num2,$base2,10);
+        echo "<script> alert('$num1($base1) + $num2($base2) = $resultado');window.location.href='../index.html'; </script>";
     }
+
+    public static function restar($num1, $num2, $base1, $base2){
+        $resultado = base_convert($num1,$base1,10) - base_convert($num2,$base2,10);
+        echo "<script> alert('$num1($base1) - $num2($base2) = $resultado');window.location.href='../index.html'; </script>";
+    }
+
+    public static function multiplicar($num1, $num2, $base1, $base2){
+        $resultado = base_convert($num1,$base1,10) * base_convert($num2,$base2,10);
+        echo "<script> alert('$num1($base1) * $num2($base2) = $resultado');window.location.href='../index.html'; </script>";
+    }
+
+    public static function dividir($num1, $num2, $base1, $base2){
+        $resultado = base_convert($num1,$base1,10) / base_convert($num2,$base2,10);
+        echo "<script> alert('$num1($base1) / $num2($base2) = $resultado');window.location.href='../index.html'; </script>";
+    }
+
 }
 ?>
