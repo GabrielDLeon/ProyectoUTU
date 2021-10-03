0e7e8e91520154c44151aa691bd164c9d55b31af
GabrielDLeon <noesgabriel7574@gmail.com>
2021-10-03 18:24:09 -0300

    Rediseño de Publicación

--- routes/publication.js
+++ routes/publication.js
@@ -78,35 +78,41 @@ router.get('/:id', authController.isLoggedIn, async (req, res) => {
     const {id} = req.params;
     await db.query('SELECT nroPublicacion, precio, titulo, descripcion, producto, cuenta_empresa.email AS vendedorEmail, cuenta_empresa.nombre AS vendedor FROM (publicacion INNER JOIN cuenta_empresa ON publicacion.vendedor = cuenta_empresa.email) WHERE nroPublicacion = ?',[id], async (error, result) => { 
         if (result.length>0){
-            await db.query('SELECT talle FROM (publicacion INNER JOIN curvas ON publicacion.nroPublicacion = curvas.publicacion) WHERE nroPublicacion = ?',[id], async (error, talles) => {
-                await db.query('SELECT porcentaje AS descuento FROM (descuento INNER JOIN publicacion ON descuento.publication = publicacion.nroPublicacion) WHERE publicacion.nroPublicacion = ?', [id], async (error, sale) => {
-                    if (sale.length>0){
-                        var valor = {
-                            descuento: sale[0].descuento,
-                            final: saleCalculator(result[0].precio, sale[0].descuento)
+            await db.query('SELECT idProducto, categoria, genero, material, marca FROM (publicacion INNER JOIN producto ON publicacion.producto = producto.idProducto) WHERE publicacion.nroPublicacion = ?',[id], async (error, product) => {
+                await db.query('SELECT talle FROM (publicacion INNER JOIN curvas ON publicacion.nroPublicacion = curvas.publicacion) WHERE nroPublicacion = ?', [id], async (error, sizes) => {
+                    // await db.query('SELECT talle FROM (publicacion INNER JOIN curvas ON publicacion.nroPublicacion = curvas.publicacion) WHERE nroPublicacion = ?', [id], async (error, colors) => {
+
+                    // })
+                    await db.query('SELECT porcentaje AS descuento FROM (descuento INNER JOIN publicacion ON descuento.publication = publicacion.nroPublicacion) WHERE publicacion.nroPublicacion = ?', [id], async (error, sale) => {
+                        if (sale.length > 0) {
+                            var valor = {
+                                descuento: sale[0].descuento,
+                                final: saleCalculator(result[0].precio, sale[0].descuento)
+                            }
+                        } else {
+                            var valor = {
+                                descuento: '',
+                                final: result[0].precio,
+                            }
                         }
-                    } else {
-                        var valor = {
-                            descuento: '',
-                            final: result[0].precio,
-                        }
-                    }
-                    const vendedor = result[0].vendedorEmail;
-                    await db.query('SELECT idPregunta, mensaje, fechaPregunta, fechaRespuesta, cuenta_empresa.nombre AS vendedor, cuenta_personal.nombre AS remitente, respuesta, cuentas.tipo FROM (publicacion INNER JOIN preguntas ON publicacion.nroPublicacion = preguntas.publicacion INNER JOIN cuenta_personal ON cuenta_personal.email = preguntas.remitente INNER JOIN cuenta_empresa ON publicacion.vendedor = cuenta_empresa.email INNER JOIN cuentas ON cuentas.email = cuenta_empresa.email) WHERE nroPublicacion = ? ORDER BY fechaPregunta DESC', [id], async (error, questions) => {
-                        await db.query('SELECT nroPublicacion, precio, titulo FROM `publicacion` WHERE vendedor = ? AND nroPublicacion != ?', [vendedor, id], (error, products) => {
-                            res.render('publication/publication', {
-                                user: req.user,
-                                title: result[0].titulo,
-                                publication: result[0],
-                                talles,
-                                valor,
-                                questions,
-                                products
+                        const vendedor = result[0].vendedorEmail;
+                        await db.query('SELECT idPregunta, mensaje, fechaPregunta, fechaRespuesta, cuenta_empresa.nombre AS vendedor, cuenta_personal.nombre AS remitente, respuesta, cuentas.tipo FROM (publicacion INNER JOIN preguntas ON publicacion.nroPublicacion = preguntas.publicacion INNER JOIN cuenta_personal ON cuenta_personal.email = preguntas.remitente INNER JOIN cuenta_empresa ON publicacion.vendedor = cuenta_empresa.email INNER JOIN cuentas ON cuentas.email = cuenta_empresa.email) WHERE nroPublicacion = ? ORDER BY fechaPregunta DESC', [id], async (error, questions) => {
+                            await db.query('SELECT nroPublicacion, precio, titulo FROM `publicacion` WHERE vendedor = ? AND nroPublicacion != ?', [vendedor, id], (error, recommendations) => {
+                                res.render('publication/NDpublication', {
+                                    user: req.user,
+                                    title: result[0].titulo,
+                                    publication: result[0],
+                                    product: product[0],
+                                    sizes,
+                                    valor,
+                                    questions,
+                                    recommendations
+                                });
                             });
                         });
                     });
                 });
-            });
+            })
         } else {
             res.redirect('/');
         }
