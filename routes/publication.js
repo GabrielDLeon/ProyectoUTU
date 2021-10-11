5f87f301f14c3cce96ec0fa1b22c41e90f227cc3
GabrielDLeon <noesgabriel7574@gmail.com>
2021-10-11 16:49:01 -0300

    Responder preguntas + pequeñas correcciones

--- routes/publication.js
+++ routes/publication.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
-  });
+});
 
 router.get('/', (req, res) => {
     res.render('publication/page', {
@@ -20,29 +20,33 @@ router.get('/', (req, res) => {
 
 // Enviar formulario de pregunta
 router.post('/question/:id', authController.isLoggedIn, async (req, res) => {
-    const { id } = req.params;
-    user = req.user;
-    const { mensaje } = req.body;
-    const currentDate = new Date();
-    const newQuestion = {
-        mensaje,
-        fechaPregunta: currentDate,
-        remitente: req.user.email,
-        publicacion: id
-    };
-    if (!mensaje) {
-        const path = '/publication/' + id + '/#seccion-preguntas';
-        return res.redirect(path);
+    if (req.user){
+        const { id } = req.params;
+        user = req.user;
+        const { mensaje } = req.body;
+        const currentDate = new Date();
+        const newQuestion = {
+            mensaje,
+            fechaPregunta: currentDate,
+            remitente: req.user.email,
+            publicacion: id
+        };
+        if (!mensaje) {
+            const path = '/publication/' + id + '/#seccion-preguntas';
+            return res.redirect(path);
+        }
+        await db.query('INSERT INTO `preguntas` (`mensaje`, `remitente`, `publicacion`, `fechaPregunta`) VALUES (?, ?, ?, ?)', [newQuestion.mensaje, newQuestion.remitente, newQuestion.publicacion, newQuestion.fechaPregunta], (error, result) => {
+            console.log('Pregunta enviada correctamente');
+            const path = '/publication/' + id + '/#seccion-preguntas';
+            res.redirect(path);
+        });
+    } else {
+        res.redirect('/login')
     }
-    await db.query('INSERT INTO `preguntas` (`mensaje`, `remitente`, `publicacion`, `fechaPregunta`) VALUES (?, ?, ?, ?)', [newQuestion.mensaje, newQuestion.remitente, newQuestion.publicacion, newQuestion.fechaPregunta], (error, result) => {
-        console.log('Pregunta enviada correctamente');
-        const path = '/publication/' + id + '/#seccion-preguntas';
-        res.redirect(path);
-    });
 })
 
 // Enviar formulario de respuesta del vendedor
-router.post('/respuesta/:id', authController.isLoggedIn, async (req, res) => {
+router.post('/answer/:id', authController.isLoggedIn, async (req, res) => {
     const { id } = req.params
     await db.query('SELECT idPregunta, nroPublicacion, mensaje, fechaPregunta, cuenta_empresa.nombre AS vendedor, cuenta_personal.nombre AS remitente, respuesta FROM (publicacion INNER JOIN preguntas ON publicacion.nroPublicacion = preguntas.publicacion INNER JOIN cuenta_personal ON cuenta_personal.email = preguntas.remitente INNER JOIN cuenta_empresa ON publicacion.vendedor = cuenta_empresa.email) WHERE idPregunta = ? ORDER BY fechaPregunta DESC', [id], async (error, questions) => {
         const nroPublicacion = questions[0].nroPublicacion
@@ -61,7 +65,8 @@ router.post('/respuesta/:id', authController.isLoggedIn, async (req, res) => {
         }
     })
 });
-        
+
+// Función que calcula el precio final a partir de un descuento
 function saleCalculator (price, sale){
     const result = price - ((price * sale) / 100);
     return result;
@@ -92,23 +97,23 @@ router.get('/:id', authController.isLoggedIn, async (req, res) => {
                                     }
                                     const vendedor = result[0].vendedorEmail;
                                     await db.query('SELECT idPregunta, mensaje, fechaPregunta, fechaRespuesta, cuenta_empresa.nombre AS vendedor, cuenta_personal.nombre AS remitente, respuesta, cuentas.tipo FROM (publicacion INNER JOIN preguntas ON publicacion.nroPublicacion = preguntas.publicacion INNER JOIN cuenta_personal ON cuenta_personal.email = preguntas.remitente INNER JOIN cuenta_empresa ON publicacion.vendedor = cuenta_empresa.email INNER JOIN cuentas ON cuentas.email = cuenta_empresa.email) WHERE nroPublicacion = ? ORDER BY fechaPregunta DESC', [id], async (error, questions) => {
-                                        await db.query('SELECT nroPublicacion, precio, titulo FROM `publicacion` WHERE vendedor = ? AND nroPublicacion != ?', [vendedor, id], (error, recommendations) => {
-                                          db.query('SELECT * from perfil WHERE email = ?', [vendedor], (error, perfil) => {
-                                            res.render('publication/page', {
-                                                user: req.user,
-                                                title: result[0].titulo,
-                                                publication: result[0],
-                                                product: product[0],
-                                                favorite: favorite[0],
-                                                colors,
-                                                sizes,
-                                                valor,
-                                                imagen,
-                                                questions,
-                                                recommendations,
-                                                perfil: perfil[0]
+                                        await db.query('SELECT nroPublicacion, precio, titulo FROM `publicacion` WHERE vendedor = ? AND nroPublicacion != ?', [vendedor, id], async (error, recommendations) => {
+                                            await db.query('SELECT * from perfil WHERE email = ?', [vendedor], (error, perfil) => {
+                                                res.render('publication/page', {
+                                                    user: req.user,
+                                                    title: result[0].titulo,
+                                                    publication: result[0],
+                                                    product: product[0],
+                                                    favorite: favorite[0],
+                                                    colors,
+                                                    sizes,
+                                                    valor,
+                                                    imagen,
+                                                    questions,
+                                                    recommendations,
+                                                    perfil: perfil[0]
+                                                });
                                             });
-                                        });
                                         });
                                     });
                                 });
@@ -123,6 +128,7 @@ router.get('/:id', authController.isLoggedIn, async (req, res) => {
     });
 });
 
+// Agregar o quitar una publicación de favoritos
 router.post('/addFavorite/:id', authController.isLoggedIn, async (req, res) => {
     if (req.user){
         const { id } = req.params;
