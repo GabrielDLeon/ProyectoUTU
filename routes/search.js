395cb2856f453280b1533adba0163e05e0d8c7b9
GabrielDLeon <noesgabriel7574@gmail.com>
2021-10-21 23:27:57 -0300

    Búsqueda mejorada

--- routes/search.js
+++ routes/search.js
@@ -9,29 +9,28 @@ const db = mysql.createConnection({
     database: process.env.DATABASE,
 });
 
-router.get('/', authController.isLoggedIn, async (req, res) => {
-    res.redirect('/');
+router.post('/', async (req, res) => {
+    const { palabra } = req.body;
+    if (palabra){
+        const path = '/search?key='+palabra;
+        res.redirect(path);
+    } else if (!palabra) {
+        res.redirect('/');
+    }
 })
 
 router.get('/', authController.isLoggedIn, async (req, res) => {
-    const { palabra } = req.body;
-    db.query('SELECT * FROM perfil WHERE perfil.email LIKE "%"?"%"', [palabra], (error, shops) => {
-        db.query('SELECT nroPublicacion, precio, titulo, precio-precio*descuento.porcentaje/100 AS descuento, descripcion, producto, fotos.imagen, cuenta_empresa.nombre AS vendedor, categoria, genero, material, marca FROM (publicacion LEFT JOIN cuenta_empresa ON publicacion.vendedor = cuenta_empresa.email LEFT JOIN fotos ON fotos.publicacion = publicacion.nroPublicacion LEFT JOIN producto ON publicacion.nroPublicacion = producto.idProducto LEFT JOIN descuento ON descuento.publication = publicacion.nroPublicacion) WHERE titulo LIKE "%"?"%" OR categoria LIKE "%"?"%" OR genero LIKE "%"?"%" OR material LIKE "%"?"%" OR marca LIKE "%"?"%" OR cuenta_empresa.nombre LIKE "%"?"%" GROUP BY nroPublicacion', [palabra, palabra, palabra, palabra, palabra, palabra], (error, recommendations) => {
-            if (recommendations.length > 0) {
-                return res.render('search', {
-                    recommendations,
-                    palabra: req.body.palabra,
-                    shops: shops,
-                    user: req.user,
-                    title: "Búsqueda"
-                });
-            } else {
-                return res.render('search', {
-                    message: 'No hay coincidencias con la busqueda',
-                    user: req.user,
-                    title: "Búsqueda"
-                });
-            }
+    const query = req.query;
+    const {key} = query;
+    db.query('SELECT * FROM perfil WHERE perfil.email LIKE "%"?"%"', [key], (error, shops) => {
+        db.query('SELECT nroPublicacion, precio, titulo, precio-precio*descuento.porcentaje/100 AS descuento, descripcion, producto, fotos.imagen, cuenta_empresa.nombre AS vendedor, categoria, genero, material, marca FROM (publicacion LEFT JOIN cuenta_empresa ON publicacion.vendedor = cuenta_empresa.email LEFT JOIN fotos ON fotos.publicacion = publicacion.nroPublicacion LEFT JOIN producto ON publicacion.nroPublicacion = producto.idProducto LEFT JOIN descuento ON descuento.publication = publicacion.nroPublicacion) WHERE titulo LIKE "%"?"%" OR categoria LIKE "%"?"%" OR genero LIKE "%"?"%" OR material LIKE "%"?"%" OR marca LIKE "%"?"%" OR cuenta_empresa.nombre LIKE "%"?"%" GROUP BY nroPublicacion', [key, key, key, key, key, key], (error, recommendations) => {
+            res.render('search', {
+                recommendations,
+                palabra: key,
+                shops: shops,
+                user: req.user,
+                title: "Búsqueda"
+            });
         });
     });
 });
